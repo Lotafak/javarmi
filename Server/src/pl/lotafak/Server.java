782afa23c4bcd42d9889bd4c6cd3784e63d20b3d
patrykk <patrykk.93@gmail.com>
2016-12-09 09:40:16 +0100

    Variable name change

--- Server/src/pl/lotafak/Server.java
+++ Server/src/pl/lotafak/Server.java
@@ -10,10 +10,10 @@ public class Server {
     public static void main(String[] args){
         HelloImpl hello = new HelloImpl();
         try {
-            HelloInt helloSkeleton = (HelloInt) UnicastRemoteObject.exportObject(hello, 0);
+            HelloInt helloStub = (HelloInt) UnicastRemoteObject.exportObject(hello, 0);
 
             Registry registry = LocateRegistry.createRegistry(1099);
-            registry.bind("Hello", helloSkeleton);
+            registry.bind("Hello", helloStub);
 
             System.out.println("Server ready");
 
